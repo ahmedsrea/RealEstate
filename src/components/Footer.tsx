4a13bf7eb68b94b737520d8fa4e77247be81018d
ahmedsrea <ahmedfast13@gmail.com>
2023-08-21 03:08:19 +0300

    Create Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full text-center bg-[#f2f4f5] p-4 text-sm">
+      All Rights Reserved 2021 ©
+    </footer>
+  );
+};
+
+export default Footer;

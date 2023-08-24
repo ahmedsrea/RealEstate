a4244863198a2fddd75e537e0435be36bc624882
ahmedsrea <ahmedfast13@gmail.com>
2023-08-24 16:29:46 +0300

    Add a black overlay to the background

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -3,13 +3,14 @@ import SearchForm from "./SearchForm";
 const Landing: React.FC = () => {
   return (
     <div
-      className="md:max-h-[570px] h-[90vh] w-full flex flex-col justify-center items-center text-center"
+      className="md:max-h-[570px] h-[90vh] w-full flex flex-col justify-center items-center text-center relative"
       style={{
         background: 'no-repeat center url("../assests/top-bg.png")',
         backgroundSize: "cover",
       }}
     >
-      <div>
+      <div className="absolute left-0 top-0 z-0 w-full h-full flex justify-center items-center backdrop-brightness-75"></div>
+      <div className="z-10">
         <h2 className="md:text-4xl text-3xl mb-4 text-white font-bold">
           Real Estate Egypt
         </h2>

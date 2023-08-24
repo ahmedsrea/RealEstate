6ecfc4bbe08d61864b733f09e1324fa421a6cd2c
ahmedsrea <ahmedfast13@gmail.com>
2023-08-24 16:34:25 +0300

    Fix the height of the navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import MobileMenu from "./MobileMenu";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="xl:bg-[#F5F7F9] bg-white w-full h-[70px] sticky top-0 left-0 z-10 shadow-md">
+    <div className="xl:bg-[#F5F7F9] bg-white w-full h-[65px] sticky top-0 left-0 z-20 shadow-md">
       <div className="xl:max-w-[1400px] w-full mx-auto flex flex-row items-center lg:justify-between py-[14px] px-[15px]">
         <div className="flex flex-row items-center lg:w-auto w-full">
           <MobileMenu />

0bacdffdf701b62a16108fc56e99cfc566c03784
ahmedsrea <ahmedfast13@gmail.com>
2023-09-02 02:27:53 +0300

    Add blog path to App file

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Compounds from "./components/compounds/Compounds";
 import Home from "./components/home/Home";
 import Footer from "./components/Footer";
 import AddItem from "./components/AddItem";
+import Item from "./components/Item";
+import ForSale from "./components/for-sale/ForSale";
+import Blog from "./components/blog/Blog";
+import NewBlog from "./components/blog/NewBlog";
+import Blogs from "./components/blog/Blogs";
 
 function App() {
   return (
@@ -13,7 +18,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/compounds" element={<Compounds />} />
+          <Route path="/for-sale" element={<ForSale />} />
           <Route path="/add-item" element={<AddItem />} />
+          <Route path="/:slug" element={<Item />} />
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/blog/:slug" element={<Blog />} />
+          <Route path="/blog/new" element={<NewBlog />} />
         </Routes>
         <Footer />
       </Router>

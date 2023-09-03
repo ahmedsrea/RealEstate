795e5ca9716c1be5c9d39afb38a729498073dcfa
ahmedsrea <ahmedfast13@gmail.com>
2023-09-03 17:57:38 +0300

    Improve the style of file input

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -40,7 +40,9 @@ const Input: React.FC<InputProps> = ({
         value={value}
         accept={accept}
         multiple={multiple}
-        className="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
+        className={`${
+          type === "file" ? "w-fit" : "w-full"
+        } p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500`}
       />
     </div>
   );

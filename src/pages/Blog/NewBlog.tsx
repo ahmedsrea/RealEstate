1c621fef26d1fd6b9c455be52cda5801ad1051e9
ahmedsrea <ahmedfast13@gmail.com>
2023-09-04 04:50:54 +0300

    Add the missing inputs to the client

--- src/pages/Blog/NewBlog.tsx
+++ src/pages/Blog/NewBlog.tsx
@@ -6,7 +6,13 @@ import Textarea from "../../components/inputs/Textarea";
 const NewBlog = () => {
   const url = "http://localhost:3000/blogs";
   const [data, setData] = useState({
+    blog_title: "",
     title: "",
+    location: "",
+    price: "",
+    status: "",
+    del_date: "",
+    dev_by: "",
     description: "",
     markdown: "",
   });
@@ -30,7 +36,13 @@ const NewBlog = () => {
       .post(
         url,
         {
+          blog_title: data.blog_title,
           title: data.title,
+          location: data.location,
+          price: data.price,
+          status: data.status,
+          del_date: data.del_date,
+          dev_by: data.dev_by,
           description: data.description,
           markdown: data.markdown,
           images: images,
@@ -48,6 +60,15 @@ const NewBlog = () => {
       New Blog
       <form className="flex flex-col w-full" onSubmit={(e) => submit(e)}>
         <Input
+          label="Blog Title"
+          htmlFor="blog_title"
+          type="text"
+          name="blog_title"
+          id="blog_title"
+          onChange={(e) => handle(e)}
+          value={data.blog_title}
+        />
+        <Input
           label="Title"
           htmlFor="title"
           type="text"
@@ -55,6 +76,51 @@ const NewBlog = () => {
           id="title"
           onChange={(e) => handle(e)}
           value={data.title}
+        />
+        <Input
+          label="Location"
+          htmlFor="location"
+          type="text"
+          name="location"
+          id="location"
+          onChange={(e) => handle(e)}
+          value={data.location}
+        />
+        <Input
+          label="Price"
+          htmlFor="price"
+          type="text"
+          name="price"
+          id="price"
+          onChange={(e) => handle(e)}
+          value={data.price}
+        />
+        <Input
+          label="Status"
+          htmlFor="status"
+          type="text"
+          name="status"
+          id="status"
+          onChange={(e) => handle(e)}
+          value={data.status}
+        />
+        <Input
+          label="Delivery Date"
+          htmlFor="del_date"
+          type="text"
+          name="del_date"
+          id="del_date"
+          onChange={(e) => handle(e)}
+          value={data.del_date}
+        />
+        <Input
+          label="Developed by"
+          htmlFor="dev_by"
+          type="text"
+          name="dev_by"
+          id="dev_by"
+          onChange={(e) => handle(e)}
+          value={data.dev_by}
         />
         <Textarea
           label="Description"

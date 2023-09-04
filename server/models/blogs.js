f05d381d7fd27e88d026b9564b388d636e2fdceb
ahmedsrea <ahmedfast13@gmail.com>
2023-09-04 04:45:08 +0300

    Add the missing inputs

--- server/models/blogs.js
+++ server/models/blogs.js
@@ -6,7 +6,22 @@ const { JSDOM } = require("jsdom");
 const dompurify = createDomPurify(new JSDOM().window);
 
 const blogSchema = new mongoose.Schema({
+  blog_title: {
+    type: String,
+  },
   title: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  status: {
+    type: String,
+  },
+  del_date: {
+    type: Number,
+  },
+  dev_by: {
     type: String,
   },
   description: {

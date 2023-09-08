cfdc06d99a5b5a81d409ef62e930e5e534fd42a2
ahmedsrea <ahmedfast13@gmail.com>
2023-09-09 02:34:48 +0300

    Update the connect component and add the sticky one and the mobile one

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,22 +1,59 @@
 import { BsFillTelephoneFill, BsWhatsapp } from "react-icons/bs";
+import { FaHeadset } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 const Connect = () => {
   return (
-    <div className="flex flex-col">
-      <h1 className="font-bold text-lg mb-4">Connect:</h1>
+    <div className="">
+      {/* Display > lg */}
+      <div className="lg:flex flex-col items-start hidden">
+        <h1 className="font-bold text-lg mb-4">Connect:</h1>
 
-      <Link
-        to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
-        className="flex flex-row items-center gap-2 text-[#19C34A] mb-3"
-      >
-        <BsWhatsapp /> +201009808986555
-      </Link>
-      <Link
-        to={`tel:+201009808986ew2a`}
-        className="flex flex-row items-center gap-2 text-[#83818A]"
-      >
-        <BsFillTelephoneFill /> +2010098089868746
-      </Link>
+        <Link
+          to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
+          className="flex flex-row items-center gap-2 text-[#19C34A] mb-3"
+        >
+          <BsWhatsapp /> +201009808
+        </Link>
+        <Link
+          to={`tel:+201009808986ew2a`}
+          className="flex flex-row items-center gap-2 text-[#83818A]"
+        >
+          <BsFillTelephoneFill /> +201009808
+        </Link>
+      </div>
+
+      {/* Display < lg */}
+      <div className="lg:hidden flex flex-row justify-evenly items-center gap-3 bg-white border border-[#ddd] rounded-md p-4">
+        <div className="flex flex-row items-center justify-center gap-3 p-4 border border-[#019DFB] rounded-lg w-[50%] text-[#019DFB] cursor-pointer">
+          <FaHeadset /> Request call
+        </div>
+        <Link
+          to={`tel:+201009808986ew2a`}
+          className="flex flex-row items-center justify-center gap-2 text-[#FB6B01] p-4 border border-[#FB6B01] rounded-lg w-[50%]"
+        >
+          <BsFillTelephoneFill /> Call Now
+        </Link>
+      </div>
+
+      {/* Stick one */}
+      <div className="fixed bottom-0 left-0 h-[65px] bg-white w-full flex flex-row justify-around">
+        <div className="flex flex-col items-center justify-center gap-2 text-[#019DFB] cursor-pointer">
+          <FaHeadset /> <span className="text-xs">Request call</span>
+        </div>
+        <Link
+          to={`tel:+201009808986ew2a`}
+          className="flex relative flex-col items-center justify-center gap-2 text-[#FB6B01]"
+        >
+          <span className="animate-[ping_2s_infinite] absolute inline-flex h-full w-full rounded-lg bg-[#FB6B01] opacity-75"></span>
+          <BsFillTelephoneFill /> <span className="text-xs">Call Now</span>
+        </Link>
+        <Link
+          to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
+          className="flex flex-col items-center justify-center gap-2 text-[#19C34A]"
+        >
+          <BsWhatsapp /> <span className="text-xs">Whatsapp</span>
+        </Link>
+      </div>
     </div>
   );
 };

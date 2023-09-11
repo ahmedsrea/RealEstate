bd40cc738e921473863d4489081394159d18779b
ahmedsrea <ahmedfast13@gmail.com>
2023-09-11 21:32:12 +0300

    chore: remove unnecessary comment

--- src/pages/Show/ForSaleSection.tsx
+++ src/pages/Show/ForSaleSection.tsx
@@ -10,7 +10,6 @@ const ForSaleSection = () => {
     return await fetch(url)
       .then((response) => response.json())
       .then((data) => setData(data));
-    // .then((data) => console.log(data));
   }
 
   useEffect(() => {

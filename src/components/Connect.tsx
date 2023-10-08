58d5302b04824cfee2b40741fa3b7bd6ebb8d23c
ahmedsrea <ahmedfast13@gmail.com>
2023-10-08 15:42:35 +0300

    feat: update the Connect component to adapt to the developer page

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,29 +1,62 @@
 import { BsFillTelephoneFill, BsWhatsapp } from "react-icons/bs";
 import { FaHeadset } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-const Connect = () => {
+
+type ConnectProp = {
+  DevPage?: boolean;
+};
+
+const Connect = ({ DevPage }: ConnectProp) => {
   return (
     <div className="">
       {/* Display > lg */}
-      <div className="lg:flex flex-col items-start hidden">
-        <h1 className="font-bold text-lg mb-4">Connect:</h1>
 
-        <Link
-          to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
-          className="flex flex-row items-center gap-2 text-[#19C34A] mb-3"
-        >
-          <BsWhatsapp /> +201009808
-        </Link>
-        <Link
-          to={`tel:+201009808986ew2a`}
-          className="flex flex-row items-center gap-2 text-[#83818A]"
-        >
-          <BsFillTelephoneFill /> +201009808
-        </Link>
-      </div>
+      {DevPage ? (
+        <div className={`lg:flex flex-row hidden`}>
+          <Link
+            to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
+            className="flex flex-row items-center gap-2 text-[#19C34A] mr-4"
+          >
+            <BsWhatsapp /> +201009808
+          </Link>
+          <Link
+            to={`tel:+201009808986ew2a`}
+            className="flex flex-row items-center gap-2 text-[#83818A] mr-4"
+          >
+            <BsFillTelephoneFill /> +201009808
+          </Link>
+          <Link
+            to={`tel:+201009808986ew2a`}
+            className="flex flex-row items-center justify-center gap-2 text-[#019DFB]"
+          >
+            <FaHeadset /> Request call
+          </Link>
+        </div>
+      ) : (
+        <div className={`lg:flex flex-col items-start hidden`}>
+          <h1 className="font-bold text-lg mb-4">Connect:</h1>
+
+          <Link
+            to={`https://api.whatsapp.com/send?phone=+2010098089862as`}
+            className="flex flex-row items-center gap-2 text-[#19C34A] mb-3"
+          >
+            <BsWhatsapp /> +201009808
+          </Link>
+          <Link
+            to={`tel:+201009808986ew2a`}
+            className="flex flex-row items-center gap-2 text-[#83818A]"
+          >
+            <BsFillTelephoneFill /> +201009808
+          </Link>
+        </div>
+      )}
 
       {/* Display < lg */}
-      <div className="lg:hidden flex flex-row justify-evenly items-center gap-3 bg-white border border-[#ddd] rounded-md p-4">
+      <div
+        className={`${
+          DevPage ? "hidden" : "lg:hidden flex"
+        } flex-row justify-evenly items-center gap-3 bg-white border border-[#ddd] rounded-md p-4`}
+      >
         <div className="flex flex-row items-center justify-center gap-3 p-4 border border-[#019DFB] rounded-lg w-[50%] text-[#019DFB] cursor-pointer">
           <FaHeadset /> Request call
         </div>

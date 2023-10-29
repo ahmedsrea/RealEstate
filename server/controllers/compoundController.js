e072cdf008b50a9ae0dbe8da518c3e9d892cdc17
ahmedsrea <ahmedfast13@gmail.com>
2023-10-29 14:01:43 +0200

    feat:server: Implement advanced filtering options, including location, project type, and more

--- server/controllers/compoundController.js
+++ server/controllers/compoundController.js
@@ -2,24 +2,38 @@ const CompoundsModel = require("../models/compounds");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllCompounds = catchAsync(async (req, res, next) => {
-  console.log(req.query);
-
-  // BUILD QUERY
-  // 1A) Filtering
   const queryObj = { ...req.query };
+  for (const key in queryObj) {
+    if (queryObj.hasOwnProperty(key) && queryObj[key] === "") {
+      delete queryObj[key];
+    }
+  }
 
-  // 1B) Advanced filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-  console.log(JSON.parse(queryStr));
+  const excludedFields = ["min_area", "max_area", "min_price", "max_price"];
+  excludedFields.forEach((el) => delete queryObj[el]);
 
-  let query = CompoundsModel.find(JSON.parse(queryStr));
+  let query = CompoundsModel.find(queryObj);
+
+  if (req.query.min_price && req.query.min_price) {
+    query = query
+      .where("price")
+      .gte(req.query.min_price)
+      .lte(req.query.max_price);
+  }
+
+  if (req.query.min_area && req.query.max_area) {
+    query = query
+      .where("unite_size")
+      .gte(req.query.min_area)
+      .lte(req.query.max_area);
+  }
 
   // EXECUTE QUERY
   const compounds = await query;
 
   res.status(200).json({
     status: "success",
+    length: compounds.length,
     data: compounds,
   });
 });

2cab741a26c5606e79d8f42bcf46c08d2eefdfa1
ahmedsrea <ahmedfast13@gmail.com>
2023-11-02 14:22:42 +0200

    feat:server: add some security add security HTTP headers limit requests from the same IP data sanitization against NoSQL query injection prevent parameter pollution

--- server/server.js
+++ server/server.js
@@ -8,9 +8,34 @@ const compoundRoutes = require("./routes/compoundRoutes");
 const devRoutes = require("./routes/devRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 150,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour",
+});
+app.use("/api", limiter);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ["min_price", "max_price", "min_area", "max_area"],
+  })
+);
 
 app.use(cors()); // fuck you
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
 process.on("uncaughtException", (err) => {

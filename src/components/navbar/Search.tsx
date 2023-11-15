1c195dd9f6ca6d24d53212c5db4d3a8773d08cd3
ahmedsrea <ahmedfast13@gmail.com>
2023-11-14 21:31:27 -0800

    feat: implement the search functionality

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    navigate(`/search?title=${search}`);
+  }
   return (
     <form
-      method=""
-      action=""
+      onSubmit={handleSubmit}
       className="relative lg:w-auto flex flex-1 lg:ml-[50px] ml-[25px] "
     >
       <input
         type="search"
         placeholder="Project name"
-        name="navSearch"
+        name="title"
+        onChange={(e) => setSearch(e.target.value)}
         className="rounded-2xl border border-[#d1d1d1] pl-3 py-[5px] placeholder:text-[#75757E] lg:w-[204px] w-[100%] focus:outline-none"
       />
-      <div className="absolute top-2 right-4 bg-white text-[#AAAAAA]">
+      <div
+        className="absolute top-2 right-4 bg-white text-[#AAAAAA] cursor-pointer"
+        onClick={handleSubmit}
+      >
         <FiSearch />
       </div>
     </form>

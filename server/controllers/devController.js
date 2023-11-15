2d1a0c45ef953d37768cce273cee1348f1eef1f6
ahmedsrea <ahmedfast13@gmail.com>
2023-11-14 20:40:07 -0800

    feat(server): add the pagination end point in the devController

--- server/controllers/devController.js
+++ server/controllers/devController.js
@@ -2,10 +2,21 @@ const DevsModule = require("../models/devs");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllDevs = catchAsync(async (req, res, next) => {
-  const devs = await DevsModule.find();
+  const total = await DevsModule.countDocuments();
+  const page = req.query.page * 1 || 1;
+  const perPage = req.query.per_page * 1 || 10;
+  const totalPages = Math.ceil(total / perPage);
+
+  const devs = await DevsModule.find()
+    .skip((page - 1) * perPage)
+    .limit(perPage);
 
   res.status(200).json({
     status: "success",
+    page,
+    per_page: perPage,
+    total,
+    total_pages: totalPages,
     data: devs,
   });
 });

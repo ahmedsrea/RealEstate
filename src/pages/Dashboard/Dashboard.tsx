7554c09db973a7b11007c1f1b13e7edb98a42d5b
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-05 00:04:29 -0800

    feat: add outlet in the dashboard instead of write all the routes

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Layout from "./Layout";
-import ManageProducts from "./Products/ManageProducts";
-import ManageBlogs from "./Blogs/ManageBlogs";
 
 export default function Dashboard() {
   return (
-    <div className="w-full absolute top-0 left-0 z-50 bg-gray-200">
+    <div className="w-full absolute top-0 left-0 z-50 bg-gray-100">
       <Layout>
-        <Routes>
-          <Route path="products" element={<ManageProducts />} />
-          <Route path="blogs" element={<ManageBlogs />} />
-        </Routes>
+        <Outlet />
       </Layout>
     </div>
   );

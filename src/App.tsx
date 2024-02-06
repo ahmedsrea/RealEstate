35c016f5e4a8b0d79f244d7faef194055cec4c07
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-05 23:45:06 -0800

    feat: update App component with the latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,18 @@ import Footer from "./components/Footer";
 import Show from "./pages/Show/Show";
 import ForSale from "./pages/For-sale/ForSale";
 import Blog from "./pages/Blog/Blog";
-import NewBlog from "./pages/Blog/NewBlog";
 import Blogs from "./pages/Blog/Blogs";
-import ManageBlogs from "./pages/Dashboard/Blogs/ManageBlogs";
 import NotFound from "./components/NotFound";
 import Developers from "./pages/developers/Developers";
 import DevPage from "./pages/developers/DevPage";
 import ScrollToTop from "./components/ScrollToTop";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import ManageProducts from "./pages/Dashboard/Products/ManageProducts";
 import NewProduct from "./pages/Dashboard/Products/NewProduct";
 import AddBlog from "./pages/Dashboard/Blogs/NewBlog";
+import WrappedManageProducts from "./pages/Dashboard/Products/WrappedManageProducts";
+import NewDeveloper from "./pages/Dashboard/Developers/NewDeveloper";
+import WrappedManageDevelopers from "./pages/Dashboard/Developers/WrappedManageDevelopers";
+import WrappedManageBlogs from "./pages/Dashboard/Blogs/WrappedManageBlogs";
 
 function App() {
   return (
@@ -30,10 +31,12 @@ function App() {
           <Route path="/compounds" element={<Compounds />} />
           <Route path="/search" element={<Compounds />} />
           <Route path="/dashboard/*" element={<Dashboard />}>
-            <Route path="products" element={<ManageProducts />} />
+            <Route path="products" element={<WrappedManageProducts />} />
             <Route path="add-product" element={<NewProduct />} />
-            <Route path="blogs" element={<ManageBlogs />} />
+            <Route path="blogs" element={<WrappedManageBlogs />} />
             <Route path="add-blog" element={<AddBlog />} />
+            <Route path="developers" element={<WrappedManageDevelopers />} />
+            <Route path="add-dev" element={<NewDeveloper />} />
           </Route>
           <Route path="/for-sale" element={<ForSale />} />
           <Route path="/blog" element={<Blogs />} />

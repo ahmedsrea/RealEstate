d0725e02bba69e7ac8ec198704cd23e16ebd8f3e
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-05 23:36:44 -0800

    feat(server): add slugify to the title of dev model to auto generate the slug from the title

--- server/models/devs.js
+++ server/models/devs.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const devsSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  slug: { type: String, required: true },
+  slug: { type: String, unique: true },
   images: { type: String, required: true },
+});
+
+devsSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
 });
 
 const DevsModule = mongoose.model("devs", devsSchema);

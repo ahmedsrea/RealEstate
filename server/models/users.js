0561f07431676d60e7dd113de0e47e8cf0aa3d76
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-08 21:06:42 -0800

    feat(server): add user model

---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your passowrd"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

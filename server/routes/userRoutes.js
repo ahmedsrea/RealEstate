a8a6a5216a6c2503e44c84d6fbb4ab2b82ad99b3
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-13 23:28:34 -0800

    feat(server): Add new API endpoints for user signup and login in userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+
+router.post("/signup", authController.signUp);
+router.post("/login", authController.login);
 
 router
   .route("/")
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
+module.exports = router;

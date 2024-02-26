0889636532a27c857ad07a2bc7a92dc2625dac75
Ahmed Sreai <ahmedfast13@gmail.com>
2024-02-25 22:31:55 -0800

    feat(server): add Cookie Parser

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -38,6 +39,7 @@ app.use(
 app.use(cors({ credentials: true, origin: true })); // fuck you
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Routes
 app.use("/api/v1/compounds", compoundRoutes);

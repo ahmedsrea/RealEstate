f4f38a71a895e87e49f35d2150c3751df79d1df7
Ahmed Sreai <ahmedfast13@gmail.com>
2024-03-10 00:23:15 -0800

    chore(server): update the userRoutes file with the latest changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
+const logoutController = require("../controllers/logoutController");
+const refreshTokenController = require("../controllers/refreshTokenController");
 
 router.post("/signup", authController.signUp);
-router.post("/login", authController.login);
+router.post("/login", authController.handlelogin);
+router.get("/logout", logoutController.handleLogout);
+router.get("/refresh", refreshTokenController.handleRefreshToken);
 
 router
   .route("/")

93ea4f4901bdac925d77b0e46f44db870ce8e66d
Ahmed Sreai <ahmedfast13@gmail.com>
2024-06-15 04:08:29 +0300

    feat: use react-hook-form for filtration on compounds page to prevent page reloads

--- src/pages/Compounds/Compounds.tsx
+++ src/pages/Compounds/Compounds.tsx
@@ -1,11 +1,63 @@
-import Filter from "./Filter";
-import ShowItems from "./ShowItems";
+import Filter, { FilterTypes } from "./Filter";
+import { FaAnglesRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import ShowCards from "../../components/ShowCards";
+import { useState } from "react";
+import PaginationNav from "../../components/PaginationNav";
+import axios from "../../api/axios";
 
 const Compounds = () => {
+  const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState({});
+
+  const { isLoading, error, data, isPreviousData } = useQuery({
+    queryKey: ["showItems", { page, filter }],
+    queryFn: () => axios.get(`/compounds?page=${page}`, { params: filter }),
+    keepPreviousData: true,
+    networkMode: "offlineFirst",
+  });
+
+  if (isLoading) return "Loading";
+  if (error) return "An error has occured" + error;
+
+  const nextPage = () => setPage((prev) => prev + 1);
+  const prevPage = () => setPage((prev) => prev - 1);
+  const pagesArray = Array(data?.data?.total_pages)
+    .fill()
+    .map((_, index) => index + 1);
+
+  const handleFilterUpdate = (newFilter: FilterTypes) => {
+    setFilter(newFilter);
+  };
+
   return (
     <div className="mt-[97px] xl:max-w-[1400px] w-full mx-auto px-[15px] flex flex-row gap-[30px] pb-5">
-      <Filter />
-      <ShowItems />
+      <Filter onFilterUpdate={handleFilterUpdate} />
+      {/* <ShowItems /> */}
+      <div className="max-w-[1018px] w-full sm:p-4 sm:border border-[#dddddd] rounded-lg">
+        <p className="flex flex-row items-center gap-2 text-sm mb-6 ml-4">
+          <Link to={"/"} className="font-light">
+            Home
+          </Link>{" "}
+          <FaAnglesRight size={10} />
+          <span className="text-[#B4BBC5]">Egypt's Compounds</span>
+        </p>
+        <h1 className="font-bold sm:text-3xl text-2xl relative mb-7">
+          <span className="">Egypt's</span> Compounds - {data?.data?.total}{" "}
+          compounds and 30810 properties for sale.
+        </h1>
+        <ShowCards data={data?.data.data} />
+        <PaginationNav
+          prevPage={prevPage}
+          nextPage={nextPage}
+          isPreviousData={isPreviousData}
+          page={page}
+          setPage={setPage}
+          pagesArray={pagesArray}
+          total_pages={data?.data?.total_pages}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Compounds/Filter.tsx
+++ src/pages/Compounds/Filter.tsx
@@ -1,4 +1,3 @@
-import { useLocation } from "react-router-dom";
 import {
   projectType,
   location,
@@ -8,124 +7,112 @@ import {
   area,
   price,
 } from "../../data/constants";
+import { useForm } from "react-hook-form";
 
-const Filter = () => {
-  const uselocation = useLocation();
-  const searchParams = new URLSearchParams(uselocation.search);
-  const selectedProjectType = searchParams.get("project_type");
-  const selectedLocation = searchParams.get("location");
-  const selectedPropertyType = searchParams.get("property_type");
-  const selectedDeveloper = searchParams.get("developer");
-  const selectedView = searchParams.get("view");
-  const selectedMinArea = searchParams.get("min_area");
-  const selectedMaxArea = searchParams.get("max_area");
-  const selectedMinPrice = searchParams.get("min_price");
-  const selectedMaxPrice = searchParams.get("max_price");
+export interface FilterTypes {
+  location: string;
+  project_type: string;
+  property_type: string;
+  developer: string;
+  view: string;
+  min_area: number;
+  max_area: number;
+  min_price: number;
+  max_price: number;
+}
+
+interface FilterProps {
+  onFilterUpdate: (filter: FilterTypes) => void;
+}
+
+const Filter = ({ onFilterUpdate }: FilterProps) => {
+  const { register, handleSubmit } = useForm<FilterTypes>();
 
   return (
     <div className="max-w-[320px] h-fit hidden lg:block p-4 border border-[#dddddd] rounded-lg">
-      <form method="" action="" className="flex flex-col">
+      <form
+        onSubmit={handleSubmit((data) => onFilterUpdate(data))}
+        className="flex flex-col"
+      >
         <label htmlFor="location">Location</label>
-        <select name="location" id="location" className="filter-select">
+        <select
+          {...register("location")}
+          id="location"
+          className="filter-select"
+        >
           <option value="">Show all</option>
           {location.map((location, index) => (
-            <option
-              value={location.value}
-              key={index}
-              selected={location.value === selectedLocation}
-            >
+            <option value={location.value} key={index}>
               {location.title}
             </option>
           ))}
         </select>
         <label htmlFor="project-type">Project Type</label>
-        <select name="project_type" id="project-type" className="filter-select">
+        <select
+          {...register("project_type")}
+          id="project-type"
+          className="filter-select"
+        >
           <option value="">Show all</option>
           {projectType.map((type, index) => (
-            <option
-              value={type.value}
-              key={index}
-              selected={type.value === selectedProjectType}
-            >
+            <option value={type.value} key={index}>
               {type.title}
             </option>
           ))}
         </select>
         <label htmlFor="property-type">Property Type</label>
         <select
-          name="property_type"
+          {...register("property_type")}
           id="property-type"
           className="filter-select"
         >
           <option value="">Show all</option>
           {propertyType.map((type, index) => (
-            <option
-              value={type.value}
-              key={index}
-              selected={type.value === selectedPropertyType}
-            >
+            <option value={type.value} key={index}>
               {type.title}
             </option>
           ))}
         </select>
         <label htmlFor="dev">Developer</label>
-        <select name="developer" id="dev" className="filter-select">
+        <select {...register("developer")} id="dev" className="filter-select">
           <option value="">Show all</option>
           {developer.map((dev, index) => (
-            <option
-              value={dev.value}
-              key={index}
-              selected={dev.value === selectedDeveloper}
-            >
+            <option value={dev.value} key={index}>
               {dev.title}
             </option>
           ))}
         </select>
         <label htmlFor="view">View</label>
-        <select name="view" id="view" className="filter-select">
+        <select {...register("view")} id="view" className="filter-select">
           <option value="">Show all</option>
           {view.map((view, index) => (
-            <option
-              value={view.value}
-              key={index}
-              selected={view.value === selectedView}
-            >
+            <option value={view.value} key={index}>
               {view.title}
             </option>
           ))}
         </select>
         <label htmlFor="area">Area</label>
         <div className="flex flex-row gap-3">
-          <select name="min_area" id="area" className="filter-select flex-1">
+          <select
+            {...register("min_area")}
+            id="area"
+            className="filter-select flex-1"
+          >
             <option value="">Min</option>
             {area.map((area, index) => (
-              <option
-                value={area.value}
-                key={index}
-                selected={
-                  area.value ===
-                  (selectedMinArea !== null
-                    ? parseFloat(selectedMinArea)
-                    : null)
-                }
-              >
+              <option value={area.value} key={index}>
                 {area.title}
               </option>
             ))}
           </select>
-          <select name="max_area" id="area" className="filter-select  flex-1">
+          <select
+            {...register("max_area")}
+            id="area"
+            className="filter-select  flex-1"
+          >
             <option value="">Max</option>
             {area.map((area, index) => (
-              <option
-                value={area.value}
-                key={index}
-                selected={
-                  area.value ===
-                  (selectedMaxArea !== null
-                    ? parseFloat(selectedMaxArea)
-                    : null)
-                }
-              >
+              <option value={area.value} key={index}>
                 {area.title}
               </option>
             ))}
@@ -133,26 +120,26 @@ const Filter = () => {
         </div>
         <label htmlFor="price">Price</label>
         <div className="flex flex-row gap-3">
-          <select name="min_price" id="price" className="filter-select flex-1">
+          <select
+            {...register("min_price")}
+            id="price"
+            className="filter-select flex-1"
+          >
             <option value="">Min</option>
             {price.map((price, index) => (
-              <option
-                value={price.value}
-                key={index}
-                selected={price.value === selectedMinPrice}
-              >
+              <option value={price.value} key={index}>
                 {price.title}
               </option>
             ))}
           </select>
-          <select name="max_price" id="price" className="filter-select flex-1">
+          <select
+            {...register("max_price")}
+            id="price"
+            className="filter-select flex-1"
+          >
             <option value="">Max</option>
             {price.map((price, index) => (
-              <option
-                value={price.value}
-                key={index}
-                selected={price.value === selectedMaxPrice}
-              >
+              <option value={price.value} key={index}>
                 {price.title}
               </option>
             ))}

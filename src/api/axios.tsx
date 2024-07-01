c3c7d14e2e63dec7c604807fccde5e97d54dc309
Ahmed Sreai <ahmedfast13@gmail.com>
2024-07-01 21:12:25 +0300

    chore: retrieve API URL from env file

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = import.meta.env.VITE_APP_API_URL;
 
 export default axios.create({
   baseURL: BASE_URL,

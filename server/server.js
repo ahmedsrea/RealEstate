1787112feb8631fd719027e8dac8c8da59e461dd
Ahmed Sreai <ahmedfast13@gmail.com>
2024-07-08 18:02:10 +0300

    chore(server): attempt to deploy server on Vercel

--- server/server.js
+++ server/server.js
@@ -12,11 +12,10 @@ const app = require("./app");
 mongoose.connect(process.env.MongoDB_URI).catch((error) => console.log(error));
 
 const port = process.env.API_PORT || 3000;
-if (process.env.API_PORT) {
-  const server = app.listen(port, () => {
-    console.log("Server running on port", process.env.API_PORT);
-  });
-}
+
+const server = app.listen(port, () => {
+  console.log("Server running on port", process.env.API_PORT);
+});
 
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! Shutting down....");

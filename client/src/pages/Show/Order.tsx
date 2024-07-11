9f7dcc10c9e31f9d39d919ab030b7f8ea67cce55
Ahmed Sreai <ahmedfast13@gmail.com>
2024-07-11 16:30:32 +0300

    style: fix the style of order component

--- client/src/pages/Show/Order.tsx
+++ client/src/pages/Show/Order.tsx
@@ -11,9 +11,9 @@ const Order = ({ title }: order) => {
         <h1 className="font-bold text-2xl text-center mb-2">{title}</h1>
         <form
           action=""
-          className="w-full flex sm:flex-row flex-col items-center justify-center gap-2"
+          className="w-full flex md:flex-row flex-col items-center justify-center gap-2"
         >
-          <div className="flex sm:flex-row flex-col flex-wrap items-center justify-center gap-2">
+          <div className="flex sm:flex-row flex-col flex-wrap sm:flex-nowrap items-center justify-center gap-2">
             <Input
               type="string"
               id="fullname"
@@ -27,7 +27,7 @@ const Order = ({ title }: order) => {
               placeholder="Phone Number"
             />
           </div>
-          <button className="bg-[#FB6B01] hover:bg-white hover:text-[#FB6B01] py-2 px-6 rounded-md transition duration-300 mb-4">
+          <button className="bg-[#FB6B01] hover:bg-white hover:text-[#FB6B01] py-2 px-6 rounded-md transition duration-300 mb-4 sm:mb-0">
             Order Now
           </button>
         </form>
